06f853e08640cf897afe80b9e237a1cdfdb5b4ee
Rohith <rohithvaidya@gmail.com>
2024-12-01 15:06:19 +0530

    added model comparison -  flags low performing model

--- src/rohith_ai_839/pipelines/model_comparison/nodes.py
+++ src/rohith_ai_839/pipelines/model_comparison/nodes.py
@@ -1,9 +1,10 @@
 from evidently.metrics import *
 from evidently.tests import *
-
+import os
+import json
 
 def compare_trained_models(sklearn_model):
-    """# We will load the previous model and compare metrics, if the current metrics are not good, we will raise error
+    # We will load the previous model and compare metrics, if the current metrics are not good, we will raise error
     # Open and read the JSON file
     with open("data/07_model_output/data_drift.json/"+os.listdir("data/07_model_output/data_drift.json")[-2]+"/data_drift.json", 'r') as file:
         old_data_drift = json.load(file)
@@ -19,6 +20,6 @@ def compare_trained_models(sklearn_model):
     if(new_score["score"] < old_score["score"]):
         raise Exception("Model Accuracy has gone down compared to the previous version")
 
-    return"""
+    return
 
-    pass
+    
