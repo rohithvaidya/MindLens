2fc535f957c6db8794c58924a711c26671069816
Divyam Choudhary <divyamchoudhary@hotmail.com>
2024-12-02 08:51:31 +0530

    Add app

--- src/rohith_ai_839/pipelines/model_comparison/nodes.py
+++ src/rohith_ai_839/pipelines/model_comparison/nodes.py
@@ -6,6 +6,7 @@ import json
 def compare_trained_models(sklearn_model):
     # We will load the previous model and compare metrics, if the current metrics are not good, we will raise error
     # Open and read the JSON file
+    print("os.listdir('.'): ", os.listdir("."))
     with open("data/07_model_output/data_drift.json/"+os.listdir("data/07_model_output/data_drift.json")[-2]+"/data_drift.json", 'r') as file:
         old_data_drift = json.load(file)
     with open("data/07_model_output/pred_drift.json/"+os.listdir("data/07_model_output/pred_drift.json")[-2]+"/pred_drift.json", 'r') as file:
